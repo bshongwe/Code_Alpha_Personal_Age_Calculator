bc68893c89438ae9a4800e40df9db0a00bd3161b
bshongwe <dru.da.beat.hustle@gmail.com>
2024-03-25 18:20:04 +0200

    controllers

--- personal_age_calculator/src/App.jsx
+++ personal_age_calculator/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { AgeCalculator }
+	from "./components/AgeCalculator"
+import { AgeResult }
+	from "./components/AgeResult"
+import {
+	differenceInDays,
+	differenceInHours,
+	differenceInMinutes,
+	differenceInMonths,
+	differenceInSeconds,
+	differenceInWeeks,
+	differenceInYears
+}
+	from "date-fns";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [age, setAge] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const calculateAge = (date) => {
+		const today = new Date();
+		const dateObject = new Date(date);
+		const ageYears = differenceInYears(today, dateObject);
+		const ageMonths = differenceInMonths(today, dateObject);
+		const ageDays = differenceInDays(today, dateObject);
+		const ageWeeks = differenceInWeeks(today, dateObject);
+		const ageHours = differenceInHours(today, dateObject);
+		const ageMinutes = differenceInMinutes(today, dateObject);
+		const ageSeconds = differenceInSeconds(today, dateObject);
+		setAge({
+			years: ageYears,
+			months: ageMonths,
+			days: ageDays,
+			weeks: ageWeeks,
+			hours: ageHours,
+			minutes: ageMinutes,
+			seconds: ageSeconds,
+		});
+	};
+
+	return (
+		<center className="container my-3">
+			<h1>Age Calculator</h1>
+			<strong>This free age calculator
+				computes age in terms of years,
+				months, weeks, days, hours,
+				minutes, and seconds,
+				given a date of birth.
+			</strong>
+			<div className="my-3">
+				<AgeCalculator calculateAge={calculateAge} />
+			</div>
+			{age && <AgeResult age={age} />}
+		</center>
+	)
 }
 
-export default App
+export default App;
